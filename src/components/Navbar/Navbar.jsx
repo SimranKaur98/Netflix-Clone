5f1e231a1f8f9bc29589c2fe209d128643fd783d
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-02-01 03:09:12 +0530

    Navbar Issue : Mobile friendly

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,92 @@
-import React, { useEffect, useRef } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-import search_icon from '../../assets/search_icon.svg'
-import bell_icon from '../../assets/bell_icon.svg'
-import profile_img from '../../assets/profile_img.png'
-import caret_icon from '../../assets/caret_icon.svg'
-import { logout } from '../../firebase'
+// import React, { useEffect, useRef } from 'react'
+// import './Navbar.css'
+// import logo from '../../assets/logo.png'
+// import search_icon from '../../assets/search_icon.svg'
+// import bell_icon from '../../assets/bell_icon.svg'
+// import profile_img from '../../assets/profile_img.png'
+// import caret_icon from '../../assets/caret_icon.svg'
+// import { logout } from '../../firebase'
+// const Navbar = () => {
+
+//   const navRef = useRef();
+//   useEffect(() => {
+//   const handleScroll = () => {
+//     if (!navRef.current) return;
+
+//     if (window.scrollY >= 80) {
+//       navRef.current.classList.add('nav-dark');
+//     } else {
+//       navRef.current.classList.remove('nav-dark');
+//     }
+//   };
+
+//   window.addEventListener('scroll', handleScroll);
+
+//   return () => {
+//     window.removeEventListener('scroll', handleScroll);
+//   };
+// }, []);
+
+
+//   return (
+//     <div className='navbar' ref={navRef}>
+//       <div className="navbar-left">
+//         <img src={logo} alt="Logo" className="navbar-logo" />
+//         <ul>
+//           <li>Home</li>
+//           <li>TV Shows</li>
+//           <li>Movies</li>
+//           <li>New & Popular</li>
+//           <li>My List</li>
+//           <li>Browse by Languages</li>
+//         </ul>
+//       </div>
+//       <div className="navbar-right">
+//         <img src={search_icon} alt="Search" className="icons" />
+//         <p>Children</p>
+//         <img src={bell_icon} alt="Notifications" className="icons" />
+//         <div className="navbar-profile">
+//           <img src={profile_img} alt="Profile" className='profile'/>
+//           <img src={caret_icon} alt="Caret" className='caret'/>
+//           <div className="dropdown">
+//           <p onClick={()=>{logout()}}>Sign Out of Netflix</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Navbar
+
+import React, { useEffect, useRef, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search_icon.svg';
+import bell_icon from '../../assets/bell_icon.svg';
+import profile_img from '../../assets/profile_img.png';
+import caret_icon from '../../assets/caret_icon.svg';
+import { logout } from '../../firebase';
+
 const Navbar = () => {
-
   const navRef = useRef();
+  const [showMenu, setShowMenu] = useState(false);
+
   useEffect(() => {
-  const handleScroll = () => {
-    if (!navRef.current) return;
-
-    if (window.scrollY >= 80) {
-      navRef.current.classList.add('nav-dark');
-    } else {
-      navRef.current.classList.remove('nav-dark');
-    }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  return () => {
-    window.removeEventListener('scroll', handleScroll);
-  };
-}, []);
-
+    const handleScroll = () => {
+      if (!navRef.current) return;
+      if (window.scrollY >= 80) navRef.current.classList.add('nav-dark');
+      else navRef.current.classList.remove('nav-dark');
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <div className='navbar' ref={navRef}>
       <div className="navbar-left">
-        <img src={logo} alt="Logo" className="navbar-logo" />
-        <ul>
+        <img src={logo} alt="Logo" />
+        <ul className={showMenu ? 'show' : ''}>
           <li>Home</li>
           <li>TV Shows</li>
           <li>Movies</li>
@@ -40,6 +94,11 @@ const Navbar = () => {
           <li>My List</li>
           <li>Browse by Languages</li>
         </ul>
+        <div className="hamburger" onClick={()=> setShowMenu(!showMenu)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
       <div className="navbar-right">
         <img src={search_icon} alt="Search" className="icons" />
@@ -49,7 +108,7 @@ const Navbar = () => {
           <img src={profile_img} alt="Profile" className='profile'/>
           <img src={caret_icon} alt="Caret" className='caret'/>
           <div className="dropdown">
-          <p onClick={()=>{logout()}}>Sign Out of Netflix</p>
+            <p onClick={()=> logout()}>Sign Out of Netflix</p>
           </div>
         </div>
       </div>
@@ -57,4 +116,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
