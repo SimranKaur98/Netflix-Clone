15a9b0dfeeba5e9718399df82a1895a54519ff54
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-02-01 01:43:44 +0530

    env variable updated

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,50 +1,70 @@
-import React, { useEffect, useState, useMemo } from 'react'
-import './Player.css'
-import back_arrow_icon from '../../assets/back_arrow_icon.png'
-import { useNavigate, useParams } from 'react-router-dom'
-
+import React, { useEffect, useState, useMemo } from "react";
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Player = () => {
-  const {id}= useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
 
   const [apiData, setApiData] = useState({
     name: "",
     key: "",
     published_at: "",
-    type: ""
+    type: "",
   });
 
-  const options = useMemo(() =>({
+  const options = useMemo(() => ({
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Y2Y4Zjk4YWNiNzUzZTg2YWE0YTg2NDViYTg3MDRmNSIsIm5iZiI6MTc2NTUyNDQwMC4yMTIsInN1YiI6IjY5M2JjM2IwZDY3MWZjMWMyM2FjOWNlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.85V1w9lS4L3JYX9S0uRUbEEqtBlH1n7Kl4J3D8O8U4w'
-    }
-  }),[]);
+      // Use environment variable here
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+    },
+  }), []);
   
+
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
-    .then(res => res.json())
-    .then(res => setApiData(res.results[0]))
-    .catch(err => console.error(err));
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options,
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.results && res.results.length > 0) {
+          setApiData(res.results[0]);
+        }
+      })
+      .catch((err) => console.error(err));
   }, [id, options]);
 
   return (
-    <div className='player'>
-
-      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
-      <iframe width="90%" height="90%" 
-      src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder="0" allowFullScreen></iframe>
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      {apiData.key && (
+        <iframe
+          width="90%"
+          height="90%"
+          src={`https://www.youtube.com/embed/${apiData.key}`}
+          title="trailer"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        />
+      )}
 
       <div className="player-info">
         <p className="player-info-date">{apiData.published_at.slice(0, 10)}</p>
         <p className="player-info-name">{apiData.name}</p>
         <p className="player-info-type">{apiData.type}</p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
